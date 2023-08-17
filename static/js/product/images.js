7a239af6ebf43468e3c0e37894638869b062a546
kristianrpo <krestrepoo@eafit.edu.co>
2023-08-16 21:53:06 -0500

    add product to database

--- static/js/product/images.js
+++ static/js/product/images.js
@@ -1,11 +1,7 @@
 var ProductImg = document.getElementById("ProductImg");
 var SmallImg = document.getElementsByClassName("small-img");
-SmallImg[0].onclick = function() {
-  ProductImg.src = SmallImg[0].src;
-}
-SmallImg[1].onclick = function() {
-  ProductImg.src = SmallImg[1].src;
-}
-SmallImg[2].onclick = function() {
-  ProductImg.src = SmallImg[2].src;
+for (let i = 0;i<SmallImg.length;i++){
+  SmallImg[i].onclick = function() {
+    ProductImg.src = SmallImg[i].src;
+  }
 }
