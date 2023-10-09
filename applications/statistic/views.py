37c39f6bb9c51692d9daa16330dea4a004b02291
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-10-08 21:21:13 -0500

    quality, warranty rating

--- applications/statistic/views.py
+++ applications/statistic/views.py
@@ -73,6 +73,68 @@ def generate_price_rating_chart(vendor):
 
     return image_base64, x
 
+
+def generate_warranty_rating_chart(vendor):
+    products = Product.objects.filter(distributed_by_product=vendor)
+    reviews_vendor = ProductRating.objects.filter(product__in=products)
+
+    calificaciones_garantia = [calificacion['warranty_rating'] for calificacion in reviews_vendor.values('warranty_rating')]
+
+    plt.figure(figsize=(8, 4))
+    colors = plt.cm.Greens(np.linspace(0.2, 1, 5))
+    bar_width = 0.5
+    x = np.arange(1, 6)
+
+    rating_counts = [calificaciones_garantia.count(rating) for rating in x]
+
+    plt.bar(x, height=rating_counts, width=bar_width, color=colors)
+    plt.xlabel('Calificación por garantía')
+    plt.ylabel('Número de Productos')
+    plt.title('Calificación por garantía de los Productos')
+
+    plt.yticks(np.arange(max(rating_counts) + 1))
+
+    plt.tight_layout(pad=2.0)
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_base64 = base64.b64encode(buffer.read()).decode()
+    buffer.close()
+
+    return image_base64, x
+
+
+def generate_quality_rating_chart(vendor):
+    products = Product.objects.filter(distributed_by_product=vendor)
+    reviews_vendor = ProductRating.objects.filter(product__in=products)
+
+    calificaciones_calidad = [calificacion['quality_rating'] for calificacion in reviews_vendor.values('quality_rating')]
+
+    plt.figure(figsize=(8, 4))
+    colors = plt.cm.Greens(np.linspace(0.2, 1, 5))
+    bar_width = 0.5
+    x = np.arange(1, 6)
+
+    rating_counts = [calificaciones_calidad.count(rating) for rating in x]
+
+    plt.bar(x, height=rating_counts, width=bar_width, color=colors)
+    plt.xlabel('Calificación por calidad')
+    plt.ylabel('Número de Productos')
+    plt.title('Calificación por calidad de los Productos')
+
+    plt.yticks(np.arange(max(rating_counts) + 1))
+
+    plt.tight_layout(pad=2.0)
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_base64 = base64.b64encode(buffer.read()).decode()
+    buffer.close()
+
+    return image_base64, x
+
 def bar_chart(request, vendor_name):
     if not request.user.is_authenticated:
         return redirect('loginaccount')
@@ -83,12 +145,16 @@ def bar_chart(request, vendor_name):
         
         category_chart_base64, categories_list = generate_category_chart(vendor)
         price_rating_chart_base64, rating_categories = generate_price_rating_chart(vendor)
+        warranty_rating_chart_base64, rating_categories = generate_warranty_rating_chart(vendor)
+        quality_rating_chart_base64, rating_categories = generate_quality_rating_chart(vendor)
 
     
         charts_data = {
             'category_chart_base64': category_chart_base64,
             'categories_list': categories_list,
             'price_rating_chart_base64': price_rating_chart_base64,
+            'warranty_rating_chart_base64': warranty_rating_chart_base64,
+            'quality_rating_chart_base64': quality_rating_chart_base64,
             'rating_categories': rating_categories,
         }
 
