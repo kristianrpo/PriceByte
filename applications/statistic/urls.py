ea0af237e53fbc1d64894050f722722bc421f6ed
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-10-11 19:50:30 -0500

    Price recommendations

--- applications/statistic/urls.py
+++ applications/statistic/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('bar_chart/<str:vendor_name>/', views.bar_chart, name='bar_chart'),
     path('quality/<str:vendor_name>/', views.quality_products, name='quality_products'),
     path('warranty/<str:vendor_name>/', views.warranty_products, name='warranty_products'),
+    path('price/<str:vendor_name>/', views.price_products, name='price_products'),
 
 ]
