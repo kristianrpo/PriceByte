4d9311c11b0edd4c6c134713b51c38ad872dc742
NicoHurtado <nicohamezquita@icloud.com>
2023-11-07 18:16:36 -0500

    Recommendations

--- applications/product/urls.py
+++ applications/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ViewProducts,SearchProducts,ViewAllProducts,DetailProduct, SearchByNlp,Search_products_NLP, Confirm_favorite, Favorites
+from .views import ViewProducts,SearchProducts,ViewAllProducts,DetailProduct, SearchByNlp,Search_products_NLP, Confirm_favorite, Favorites, Recomendations
 app_name = "product_app"
 urlpatterns = [
     path('product/', SearchProducts.as_view(),name = "search_product"),
+    path('product/recommendations/', Recomendations.as_view(), name='product_recommendations'),
     path('product/<int:pk>/', DetailProduct.as_view(), name='detail_product'),
     path('product/viewproducts',ViewProducts.as_view(), name = "view_product"),
     path('product/view_all_products',ViewAllProducts.as_view(), name = "view_all_products"),
