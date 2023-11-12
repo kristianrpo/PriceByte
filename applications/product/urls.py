4989f9e1b83564858e4da37839c7caa5dff1a8d6
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-11-11 22:29:18 -0500

    new home

--- applications/product/urls.py
+++ applications/product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ViewProducts,SearchProducts,ViewAllProducts,DetailProduct, SearchByNlp,Search_products_NLP, Confirm_favorite, Favorites, Recomendations, upload_csv
 app_name = "product_app"
 urlpatterns = [
-    path('product/', SearchProducts.as_view(),name = "search_product"),
+    path('', SearchProducts.as_view(),name = "search_product"),
     path('product/recommendations/', Recomendations.as_view(), name='product_recommendations'),
     path('product/<int:pk>/', DetailProduct.as_view(), name='detail_product'),
     path('product/viewproducts',ViewProducts.as_view(), name = "view_product"),
