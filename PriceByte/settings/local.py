2610b87dfe1a85f796eb7df6225b43a15ab45427
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-11-12 15:53:40 -0500

    ip

--- PriceByte/settings/local.py
+++ PriceByte/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.225.143.2']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
